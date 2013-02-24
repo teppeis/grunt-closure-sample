a9b83c1096a0b0b4d122bf726f8a2546bb8f43b4
teppeis <teppeis@gmail.com>
2013-02-24 22:11:34 +0900

    Compile setting

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     nodeunit: {
-      files: ['test/**/*_test.js'],
+      files: ['test/**/*_test.js']
     },
     jshint: {
       options: {
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
       },
       test: {
         src: ['test/**/*.js']
-      },
+      }
     },
     watch: {
       gruntfile: {
@@ -28,21 +28,63 @@ module.exports = function(grunt) {
       },
       lib: {
         files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
+        tasks: ['jshint:lib', 'nodeunit', 'closureCompiler']
       },
       test: {
         files: '<%= jshint.test.src %>',
         tasks: ['jshint:test', 'nodeunit']
-      },
+      }
     },
+    closureCompiler: {
+      options: {
+        // [REQUIRED] Path to closure compiler
+        compilerFile: 'node_modules/fast-closure-compiler/closure-compiler/compiler.jar',
+
+        // [OPTIONAL] set to true if you want to check if files were modified
+        // before starting compilation (can save some time in large sourcebases)
+        checkModified: false,
+
+        // [OPTIONAL] Set Closure Compiler Directives here
+        compilerOpts: {
+          /**
+           * Keys will be used as directives for the compiler
+           * values can be strings or arrays.
+           * If no value is required use null
+           *
+           * The directive 'externs' is treated as a special case
+           * allowing a grunt file syntax (<config:...>, *)
+           *
+           * Following are some directive samples...
+           */
+          compilation_level: 'ADVANCED_OPTIMIZATIONS',
+          //externs: [],
+          define: ["'goog.DEBUG=false'"],
+          warning_level: 'verbose',
+          //jscomp_off: [],
+          output_wrapper: "'(function(){%output%}).call(this);'"
+        }
+
+      },
+
+      // any name that describes your task
+      targetName: {
+        // [OPTIONAL] Target files to compile. Can be a string, an array of strings
+        // or grunt file syntax (<config:...>, *)
+        src: 'lib/closure.js',
+
+        // [OPTIONAL] set an output file
+        dest: 'dist/closure.js'
+      }
+    }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-closure-tools');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+  grunt.registerTask('default', ['jshint', 'nodeunit', 'closureCompiler']);
 
 };
